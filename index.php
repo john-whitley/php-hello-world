0d6068508d5d93c36186a480c03d085ecea0151a
John Whitley <john.whitley@berea.eu>
2014-06-12 11:50:46 +0100

    Added a really simple php file for testing a php server with.wq

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+  </head>
+  <body>
+    <h1>Hello World</h1>
+    <p>This is a demonstration page that proves that this <q>Hello World!</q> application works.</p>
+  </body>
+</html>
